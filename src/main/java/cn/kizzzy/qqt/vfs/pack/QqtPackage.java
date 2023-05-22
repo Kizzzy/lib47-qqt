4091c7e526b4d6e84b5e298b19514a4861e739b7
Kizzzy <xuhua0174@163.com>
2023-05-22 21:26:14 +0800

    [qqt] * update IStreamGetterFactory

--- src/main/java/cn/kizzzy/qqt/vfs/pack/QqtPackage.java
+++ src/main/java/cn/kizzzy/qqt/vfs/pack/QqtPackage.java
@@ -16,7 +16,7 @@ import cn.kizzzy.vfs.stream.FileStreamGetterFactory;
 public class QqtPackage extends IdxPackage {
     
     public QqtPackage(String root, ITree tree) {
-        this(tree, new FileStreamGetterFactory(root));
+        this(tree, new FileStreamGetterFactory(root, tree));
     }
     
     public QqtPackage(ITree tree, IStreamGetterFactory factory) {
